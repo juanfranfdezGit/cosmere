bcab66baa08eb33b4cef292d791720dd1b930635
juanfranfdezGit <jfranromfdez@gmail.com>
2025-11-04 12:41:25 +0100

    feat: scadrial link on map

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,13 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { RosharLink } from './roshar-link/roshar-link';
+import { ScadrialLink } from './scadrial-link/scadrial-link';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, RosharLink],
+  imports: [RouterOutlet, RosharLink, ScadrialLink],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
 export class App {
-    
+
 }
