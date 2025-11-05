6d7090c6d9b87d17e8eb75f1b72cf8bb1871f2fd
juanfranfdezGit <jfranromfdez@gmail.com>
2025-11-05 16:03:34 +0100

    feat: cosmere map

--- src/app/app.ts
+++ src/app/app.ts
@@ -3,10 +3,11 @@ import { RouterOutlet } from '@angular/router';
 import { Navbar } from './navbar/navbar';
 import { Origin } from './origin/origin';
 import { Break } from './break/break';
+import { Map } from './map/map';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Navbar, Origin, Break],
+  imports: [RouterOutlet, Navbar, Origin, Break, Map],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
