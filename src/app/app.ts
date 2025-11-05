04b4f9fad6de76fe8ce7e33e8b2ee3e58263cdd3
juanfranfdezGit <jfranromfdez@gmail.com>
2025-11-05 11:42:13 +0100

    feat: break section

--- src/app/app.ts
+++ src/app/app.ts
@@ -2,10 +2,11 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Navbar } from './navbar/navbar';
 import { Origin } from './origin/origin';
+import { Break } from './break/break';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Navbar, Origin],
+  imports: [RouterOutlet, Navbar, Origin, Break],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
---
+++ src/app/break/break.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-break',
+  imports: [],
+  templateUrl: './break.html',
+  styleUrl: './break.scss',
+})
+export class Break {
+
+}
