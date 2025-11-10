5f0076da98bb9a3bcb9a56a3ccb8d89e04da08a4
juanfranfdezGit <jfranromfdez@gmail.com>
2025-11-10 22:17:24 +0100

    feat: community section init

--- src/app/home/home.ts
+++ src/app/home/home.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { Animation } from './animation/animation';
+import { Community } from './community/community';
 
 @Component({
   selector: 'app-home',
-  imports: [Animation],
+  imports: [Animation, Community],
   templateUrl: './home.html',
   styleUrl: './home.scss',
 })
