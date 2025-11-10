93c7759e610937adbfb351185c6a66c1f15df454
juanfranfdezGit <jfranromfdez@gmail.com>
2025-11-10 17:06:43 +0100

    feat: animation in home, luthadel ready

---
+++ src/app/home/animation/animation.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit, ChangeDetectorRef } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-animation',
+  imports: [CommonModule],
+  templateUrl: './animation.html',
+  styleUrl: './animation.scss',
+})
+export class Animation implements OnInit {
+
+  activeDiv = 1;
+
+  constructor(private cd: ChangeDetectorRef) {}
+
+  ngOnInit() {
+    setInterval(() => {
+      this.activeDiv = this.activeDiv === 3 ? 1 : this.activeDiv + 1;
+      this.cd.detectChanges();
+    }, 4000);
+  }
+  
+}
--- src/app/home/home.ts
+++ src/app/home/home.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
+import { Animation } from './animation/animation';
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  imports: [Animation],
   templateUrl: './home.html',
   styleUrl: './home.scss',
 })
+
 export class Home {
 
 }
