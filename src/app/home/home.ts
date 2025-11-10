43e1067d92ec9607a31bf9476df8f0d991d6c4e1
juanfranfdezGit <jfranromfdez@gmail.com>
2025-11-10 22:38:41 +0100

    feat: news

--- src/app/home/home.ts
+++ src/app/home/home.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { Animation } from './animation/animation';
 import { Community } from './community/community';
+import { News } from './news/news';
 
 @Component({
   selector: 'app-home',
-  imports: [Animation, Community],
+  imports: [Animation, Community, News],
   templateUrl: './home.html',
   styleUrl: './home.scss',
 })
